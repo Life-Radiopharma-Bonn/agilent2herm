339e47cb82c9596e22dcd56d35d494dbaeeee58f
Volker Mauel <v.mauel@liferadiopharma.com>
2023-09-06 11:16:36 +0200

    telnet mist ignorieren, wir sprechen plain mit der karre

--- telnetserver.py
+++ telnetserver.py
@@ -1,15 +1,23 @@
 import socket
+import time
 
 HOST = "10.1.2.181"
+SUBNET_MASK="255.255.255.0"
+GATEWAY="10.1.2.1"
 PORT = 23 # this needs to be 23 since agilent expects tehre to be a client
 
 
 def readMsg(conn):
     buf = ""
-    while "\r" not in buf:
+    while "\n" not in buf:
         tmp = conn.recv(1)
+        if tmp == b'\xff':
+            print("received b'\xff' - ignoring upcoming 2 bytes")
+            # zwei weitere byte lesen und alle drei verwerfen
+            conn.recv(2)
+            continue
         try:
-            #print("{tmp}" + " " + str(tmp))
+            print("{tmp}" + " " + str(tmp))
             buf = buf + tmp.decode("ascii")
             #print(f"buffer: {buf}")
         except:
@@ -19,19 +27,32 @@ def readMsg(conn):
     return buf.strip() #nur trimmed strings zurückgeben
 
 def sendHeader(conn):
-    HEADER = """\r\n===\r\n""".encode("ascii") + b'\xff\xfc\x01'+"""Agilent 35900 Series II\r\nPlease type "?" for HELP, or "/" for current settings\r\n""".encode("ascii")
+    HEADER = """\r\n===""".encode("ascii") + b'\xff\xfc\x01'+"""Agilent 35900 Series II\r\nPlease type "?" for HELP, or "/" for current settings\r\n""".encode("ascii")
 
-    print("Sending Header {HEADER}",flush=True)
+    print(f"Sending Header {HEADER}",flush=True)
     conn.sendall(HEADER)
 
     conn.sendall(b'\x3e') #sends ">" to indicate a prompt
 
 def quitCommand(conn):
     print("QuitCommand")
-    exit()
+    conn.close()
 
 def slashCommand(conn):
     print("SlashCommand")
+    RESPONSE = ""
+    RESPONSE = RESPONSE + "   ===JetDirect Telnet Configuration===\n\r"
+    RESPONSE = RESPONSE + "Firmware Rev.: E.02.04.32\r\n"
+    RESPONSE = RESPONSE + "MAC Address: 00:aa:bb:cc:dd:ee\r\n"
+    RESPONSE = RESPONSE + "Config By: USER SPECIFIED\r\n\r\n"
+    RESPONSE = RESPONSE + "IP Address: "+HOST+"\r\n"
+    RESPONSE = RESPONSE + "Subnet Mask: "+SUBNET_MASK+"\r\n"
+    RESPONSE = RESPONSE + "Default Gateway: "+GATEWAY+"\r\n"
+    RESPONSE = RESPONSE + "DHCP Config: Disabled\r\n"
+    RESPONSE = RESPONSE + ">"
+    conn.sendall(RESPONSE.encode("ascii"))
+    print(f"Sending response {RESPONSE}")
+
     #print all info strings
     pass
 
@@ -44,22 +65,23 @@ def questionmarkCommand(conn):
 with socket.socket() as serversock:
     serversock.bind((HOST,PORT))
     serversock.listen()
-    conn, addr = serversock.accept() #this is blocking
-    with conn:
-        print(f"New Connection from client {addr}")
-        
-        ###send header
-        sendHeader(conn)
-        
-        #first message is nonsense, so take it and remove it
-        readMsg(conn)
-
-        while True:
-            data = readMsg(conn)
-            match data:
-                case "quit":
-                    quitCommand(conn)
-                case "/":
-                    slashCommand(conn)
-                case "?":
-                    questionmarkCommand(conn)
+    while True:
+        conn, addr = serversock.accept() #this is blocking
+        with conn:
+            print(f"New Connection from client {addr}")
+            
+            ###send header
+            sendHeader(conn)
+            
+            #tmp = readMsg(conn)
+    
+            while True:
+                data = readMsg(conn)
+                match data:
+                    case "quit":
+                        quitCommand(conn)
+                        break
+                    case "/":
+                        slashCommand(conn)
+                    case "?":
+                        questionmarkCommand(conn)
