c067bf825e87c292b887d1b7b654206f53507c1f
Volker Mauel <v.mauel@liferadiopharma.com>
2023-09-07 09:03:14 +0200

    wir bekommen daten rein

--- instrumentcommunication.py
+++ instrumentcommunication.py
@@ -1,11 +1,16 @@
 import socket
 import time
+import random
+import datetime
+
+from multiprocessing import Process, Queue
 
 HOST = "10.1.10.101"
 PORT = 9100 # this needs to be 23 since agilent expects tehre to be a client
 
-COUNTER=0
 READCOUNTER=0
+
+START = datetime.datetime.now()
 
 def readMsg(conn):
     buf = ""
@@ -37,8 +42,7 @@ def SYBP(conn):
     conn.sendall(HEADER)
 
 def ARBM(conn,data):
-    HEADER = """ARSP\nARRS\nARTM OFF\nAVRS\nAVSP\nARBM ?\n""".encode("ascii")
-    print(f"Sending ARBM {HEADER}",flush=True)
+    print(f"Sending ARBM {data}",flush=True)
     conn.sendall((data+"\n").encode("ascii"))
 
 def AVTS(conn,data):
@@ -51,27 +55,64 @@ def ARSS(conn,data):
     print(f"Sending ARSS {HEADER}",flush=True)
     conn.sendall(HEADER.encode("ascii"))
 
-def AVSS(conn,data):
-    global COUNTER
-    COUNTER = COUNTER + 1
-    HEADER = """AVSS ON, 0, 5, 0, """ + str(int(COUNTER)) + """\n"""
+AVSSc=1
+def AVSS(conn,data,q):
+    global AVSSc
+    global START
+    delta = str(int((datetime.datetime.now()-START).total_seconds()*1000))
+    HEADER = """AVSS ON, 0, 5, """+str(q.qsize())+""", """ + delta + """\n"""
+    #HEADER = """AVSS ON, 0, 5, 2, """ + delta + """\n"""
     print(f"Sending AVSS {HEADER}",flush=True)
     conn.sendall(HEADER.encode("ascii"))
 
 def TTSS(conn,data):
-    HEADER = """TTSS """+data.split(" ")[1]+""", ENABLED, -1, 0\n""".encode("ascii")
+    HEADER = """TTSS """+data.split(" ")[1]+""", ENABLED, -1, 0\n"""
     print(f"Sending TTSS {HEADER}",flush=True)
     conn.sendall(HEADER.encode("ascii"))
 
-def AVRD(conn,data):
-    global READCOUNTER
-    READCOUNTER = READCOUNTER+1
-    HEADER = """AVRD HEX, 001;0023 """+upper(hex(READCOUNTER))+"""\n""".encode("ascii")
+def AVRD(conn,data,q):
+    qsize = q.qsize()
+
+    DELIMITER="0023"
+    HEADER = """AVRD HEX, 00"""+str(qsize)+""";"""
+    for i in range(0,qsize):
+        val_str = str(hex(q.get())[2:]).upper().zfill(8)
+        HEADER = HEADER + val_str
+    HEADER = HEADER + "\n"
     print(f"Sending AVRD {HEADER}",flush=True)
+    print(str(HEADER.encode("ascii")) + "\t" + str(len(str(HEADER.encode("ascii")))))
     conn.sendall(HEADER.encode("ascii"))
 
-def AVSL(conn,data):
-    print(f"Received AVSL (sleep?) {data}",flush=True)
+def AVDF(conn,data):
+    global AVSSc
+    AVSSc = int(data.split(" ")[2])
+    #KEINE ANTWORT
+
+def ATRD(conn,data):
+    HEADER = """ATRD 255\n"""
+    print(f"Sending ATRD(1) {HEADER}",flush=True)
+    conn.sendall(HEADER.encode("ascii"))
+    
+def AVSL(conn,data,q):
+    i = data.split(" ")[1]
+    if i == "?":
+        HEADER = """AVSL 1000\n"""
+        print(f"Sending AVSL {HEADER}",flush=True)
+        conn.sendall(HEADER.encode("ascii"))
+    else:
+        print(f"Received AVSL (sleep?) {data}",flush=True)
+        #time.sleep(1)
+        AVRD(conn,data,q)
+
+def herm_dummy_value_gen(q):
+    print("starting herm dummy value gen")
+    start_val = 0
+    incr = 2**16
+    while True and q.qsize() < 100:
+        print("herm side qsize: " +str(q.qsize()))
+        q.put(start_val)
+        start_val = start_val + incr
+        time.sleep(1)
 
 with socket.socket() as serversock:
     serversock.bind((HOST,PORT))
@@ -85,6 +126,11 @@ with socket.socket() as serversock:
         with conn:
             print(f"New Connection from client {addr}")
             
+            print("Starting 'measurement' thread for this client to enqueue values")
+            q = Queue()
+            p = Process(target=herm_dummy_value_gen, args=(q,))
+            p.start()
+
             COUNTER=0
             READCOUNTER=0
     
@@ -105,13 +151,15 @@ with socket.socket() as serversock:
                     case "AVTS":
                         AVTS(conn, data)
                     case "AVSS":
-                        AVSS(conn, data)
+                        AVSS(conn, data, q)
                     case "AVDF":
-                        AVSS(conn, data)
+                        AVDF(conn, data)
                     case "AVRD":
-                        AVRD(conn, data)
+                        AVRD(conn, data, q)
                     case "AVSL":
-                        AVSL(conn, data)
+                        AVSL(conn, data, q)
+                    case "ATRD":
+                        ATRD(conn, data)
 
                     case "TTSS":
                         TTSS(conn, data)
